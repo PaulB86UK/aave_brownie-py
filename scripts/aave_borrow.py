c5882d2ab2a58870784f20dfe6394851fc2ad230
CrypptoPaul <84225765+CrypptoPaul@users.noreply.github.com>
2022-02-12 18:02:01 +0000

    update 12-02-2022

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,27 +1,43 @@
 from scripts.pp_functions import get_account
 from brownie import network, config, interface
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
     account = get_account()
-    erc2_address = config["networks"][network.show_active()]["weth_token"]
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active in ["mainnet-fork"]:
         get_weth()
     lending_pool = get_lending_pool()
+    print(lending_pool)
+    ## Approve sending out ERC20 tokens
+    approve_erc20(
+        amount, lending_pool.address, erc20_address, account
+    )  # lending_pool.address is the spender, erc20 is the address of the token, and my account addres?
 
 
 def get_lending_pool():
     # This part is to get the address of the lending Pool
     # lending_pool_addresses_provider is the contract that returns the address
-    lending_pool_addresses_provider = interface.IlendingPoolAddressesProvider(
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
         config["networks"][network.show_active()]["lending_pool_addresses_provider"]
     )
     # this is the function to store the address
     # function getLendingPool() external view returns (address);
 
-    lending_pool_addresses = lending_pool_addresses_provider.getLendingPool()
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
 
-    # abi
 
-    # adress check!
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 Token")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
+    return tx
